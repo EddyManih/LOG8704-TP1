7966146df59f53b378bea62a3c851e79fd766ec1
MissChungus <telesanti58@hotmail.com>
2024-09-24 15:53:55 -0400

    added possibility to delete only selected object

--- Assets/Scripts/DeleteButtonManager.cs
+++ Assets/Scripts/DeleteButtonManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class DeleteButtonManager : MonoBehaviour
 {
@@ -15,11 +16,27 @@ public class DeleteButtonManager : MonoBehaviour
         set => m_ObjectSpawner = value;
     }
 
+    [SerializeField]
+    XRInteractionGroup m_InteractionGroup;
+    public XRInteractionGroup interactionGroup
+    {
+        get => m_InteractionGroup;
+        set => m_InteractionGroup = value;
+    }
+
     private void Awake()
     {
         buttonComponent = gameObject.GetComponent<Button>();
     }
 
+    public void DeleteObject() {
+        var currentFocusedObject = m_InteractionGroup.focusInteractable;
+        if (currentFocusedObject != null)
+        {
+            Destroy(currentFocusedObject.transform.gameObject);
+        }
+        else ClearAllObjects();
+    }
     public void ClearAllObjects()
     {
         foreach (Transform child in m_ObjectSpawner.transform)
